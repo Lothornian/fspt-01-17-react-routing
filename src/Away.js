a7111f6caf70c449e2e5c7ccc018ca05ce8dfe12
Kyle Hunt <KyleRHunt@Gmail.com>
2017-04-25 18:57:46 -0600

    Added examples of links to other routes

--- src/Away.js
+++ src/Away.js
@@ -7,6 +7,8 @@ export default class Away extends Component {
     return (
       <div>
         <h1>Away</h1>
+        <p>Did you mean to go <a href='http://localhost:3000/' >Home</a>?</p>
+        <p>Or to the <a href='http://localhost:3000/NotFound' >Not found page</a>?</p>
       </div>
     )
   }
